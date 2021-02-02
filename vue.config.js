7fc97275aca3c06d4132c0a906289c85e8a3d436
徐晨凯 <xuchenkai_eb@163.com>
2021-02-02 14:35:13 +0800

    feat: test

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ module.exports = {
         // 指定检测的文件
         files: ["./src/**/*.{vue,css,less,scss}"],
         // 启动自动修复
-        fix: true,
-        cache: true // 启用缓存
+        fix: true
+        // cache: true // 启用缓存
       }
     ]);
   }
